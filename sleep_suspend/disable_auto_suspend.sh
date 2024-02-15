ade9b4f33419212e9b14594c9847b0996f51646b
Nicholas T. Eisenberg <DEVNET\eisenbergn@stl-b324166.dev.net>
2024-02-14 22:04:56 -0800

    some prevent sleep commands

--- sleep_suspend/disable_auto_suspend.sh
+++ sleep_suspend/disable_auto_suspend.sh
@@ -5,8 +5,13 @@ gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-typ
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 
 # To check the setting of auto-suspend when unplugged
-gsettings get org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+gsettings get org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type
 
 # To check the setting of disable auto-suspend when plugged
-gsettings get org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings get org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 
 
+# chatgpt said this should work
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# chatgpt said this will undue the previous
+sudo systemctl unmask sleep.target suspend.target hibernate.target hybrid-sleep.target
